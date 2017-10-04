8c76adcee7f83786f1874cc3fe8dbb9c813e31cb
Maukoell <Mauricio.koell@students.htlinn.ac.at>
2017-10-04 13:02:54 +0200

    Initial Commit

---
+++ Binärsuche/src/Binaersuche.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Binaersuche {
+
+	public void listeFuellen(int[] liste) {
+
+		for (int i = 0; i < liste.length; i++) {
+			liste[i] = i + 1;
+		}
+	}
+
+	public void suche(int[] list, int min, int max, int suchZahl) {
+
+		int temp = (min + max) /2;
+		int suche = 1;
+
+		while (list[temp - 1] != suchZahl) {
+			suche++;
+			temp = (min + max) / 2;
+			System.out.println("Stelle: " + temp);
+			if (list[temp - 1] < suchZahl) {
+				min = temp + 1;
+			} else {
+				max = temp - 1;
+			}
+
+		} 
+
+		System.out.println();
+		System.out.println("Die gesuchte Zahl " + suchZahl + " wurde gefunden.");
+		System.out.println("Es wurden " + suche + " Durchläufe benötigt.");
+
+	}
+
+	public static void main(String[] args) {
+		Binaersuche b = new Binaersuche();
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Größe der Liste: ");
+		int groesse = s.nextInt();
+		int[] list = new int[groesse];
+		System.out.println("Gesuchte Zahl: ");
+		int suchZahl = s.nextInt();
+
+		b.listeFuellen(list);
+		b.suche(list, 1, list.length, suchZahl);
+		s.close();
+
+	}
+}
